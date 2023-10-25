3c19c0626c8ec3704b5038748d22bd979425e3ed
Muhammad Faried Risky <fariedrisky@gmail.com>
2023-10-26 00:14:27 +0700

    fetch api best seller

--- src/pages/guest/HomePage/BestSellerPage/index.tsx
+++ src/pages/guest/HomePage/BestSellerPage/index.tsx
@@ -1,9 +1,32 @@
-// import { Link } from 'react-router-dom';
-import { cardData } from './constants';
-// import hero from '~/assets/hero.png';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import hero2 from '~/assets/hero2.png';
 
+interface Restaurant {
+    _id: string;
+    nama: string;
+    alamat: string;
+    kota: string;
+    gambarRestaurant: string;
+    // Definisikan properti lainnya sesuai dengan respons API
+}
+
 export const BestSellerPage = () => {
+    const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://sdg-12-b-backend-production.up.railway.app/api/restaurant/mostSells');
+                setRestaurants(response.data.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <div className="hero h-96" style={{ backgroundImage: `url(${hero2})` }}>
@@ -19,19 +42,23 @@ export const BestSellerPage = () => {
                     <span>Best Seller</span>
                 </div>
                 <div className='flex flex-wrap justify-center items-center gap-5 mt-10'>
-                    {cardData.map((card, index) => (
-                        <div key={index}>
-                            <div className="card  w-56 h-56 bg-base-100 border hover:shadow-xl mb-8 transition-transform transform hover:scale-105 duration-300 ease-in-out">
-                                <figure className="px-10 pt-10">
-                                    <img src={card.image} alt={card.title} className="w-20" />
-                                </figure>
-                                <div className="card-body items-center text-center">
-                                    <h2 className="card-title">{card.title}</h2>
+                    {Array.isArray(restaurants) && restaurants.length > 0 ? (
+                        restaurants.map((restaurant, index) => (
+                            <div key={index}>
+                                <div className="card w-72 h-90 bg-base-100 border hover:shadow-xl mb-8 transition-transform transform hover:scale-105 duration-300 ease-in-out">
+                                    <figure className="px-2 pt-2">
+                                        <img src={restaurant.gambarRestaurant} alt={restaurant.nama} className="w-full bg-gray-100 rounded-xl" />
+                                    </figure>
+                                    <div className="card-body items-center text-center">
+                                        <h2 className="card-title">{restaurant.nama}</h2>
+                                        <p className="text-sm">{restaurant.alamat}, {restaurant.kota}</p>
+                                    </div>
                                 </div>
                             </div>
-                            <button className="btn btn-primary w-fill">Order Now</button>
-                        </div>
-                    ))}
+                        ))
+                    ) : (
+                        <p>No restaurants available</p>
+                    )}
                 </div>
             </div>
         </>
