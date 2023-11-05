47902b923672ab1ba8d1d4200e13c76cadb9f539
reyangga9 <reyangga5@gmail.com>
2023-11-05 13:36:10 +0700

    test

--- src/pages/guest/RestaurantPage/DetailPage/types.ts
+++ src/pages/guest/RestaurantPage/DetailPage/types.ts
@@ -1,32 +1,31 @@
 export interface Restaurant {
-    _id: string;
-    nama: string;
-    category: string[];
-    alamat: string;
-    kota: string;
-    gambarRestaurant: string;
-    rating: Rating[];
-    // Definisikan properti lainnya sesuai dengan respons API
+  _id: string;
+  nama: string;
+  category: string[];
+  alamat: string;
+  kota: string;
+  gambarRestaurant: string;
+  rating: Rating[];
+  // Definisikan properti lainnya sesuai dengan respons API
 }
 export interface Rating {
-    name: string;
-    rating: number;
-    comment?: string;
-    _id: string;
+  name: string;
+  rating: number;
+  comment?: string;
+  _id: string;
 }
 
 export interface ColorCache {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 export interface Food {
-    _id: string;
-    makanan: string;
-    tanggalExpired: string;
-    gambarMakanan: string;
-    harga: number;
-    restoId: string;
-    stokMakanan: number;
-    discountPercentage: number;
+  _id: string;
+  makanan: string;
+  tanggalExpired: string;
+  gambarMakanan: string;
+  harga: number;
+  restoId: string;
+  stokMakanan: number;
+  discountPercentage: number;
 }
-
