2d68c78d45319a9590c3970a3e3ae80e616b723f
reyangga9 <reyangga5@gmail.com>
2023-11-07 19:12:49 +0700

    fix Restaurant

--- src/pages/guest/RestaurantPage/DetailPage/RestoProfileSection/types.ts
+++ src/pages/guest/RestaurantPage/DetailPage/RestoProfileSection/types.ts
@@ -1,8 +1,8 @@
-import { Restaurant } from "../types"; // Import your Restaurant type definition
+import { Restaurant } from "~/hook/useRestaurantHook";
 
 export interface RestoProfileSectionProps {
-    restaurant: Restaurant | null;
-    setImageLoaded: React.Dispatch<React.SetStateAction<boolean>>; // Define the prop type for setImageLoaded
-    imageLoaded: boolean;
-    loading: boolean;
+  restaurant: Restaurant | null;
+  setImageLoaded: React.Dispatch<React.SetStateAction<boolean>>; // Define the prop type for setImageLoaded
+  imageLoaded: boolean;
+  loading: boolean;
 }
