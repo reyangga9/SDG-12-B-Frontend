b39dde0cf0b1227acce76675ee7afc60fa45bb6f
reyangga9 <reyangga5@gmail.com>
2023-11-15 00:53:25 +0700

    deleting warning

--- src/pages/auth/CheckoutPage/index.tsx
+++ src/pages/auth/CheckoutPage/index.tsx
@@ -3,7 +3,6 @@ import { ArrowLeft, BadgeDollarSign, Minus, Plus } from "lucide-react";
 import useCartStore from "~/store/cartStore";
 import { BiSolidFoodMenu } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
 const CheckoutPage = () => {
   const {
