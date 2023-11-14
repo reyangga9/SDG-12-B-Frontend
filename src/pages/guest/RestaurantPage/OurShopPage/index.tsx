c0ffe4733a6ab1d58ffc2eaadc32f0f482f7defd
Muhammad Faried Risky <fariedrisky@gmail.com>
2023-11-14 22:18:45 +0700

    improve search on ourshop page

--- src/pages/guest/RestaurantPage/OurShopPage/index.tsx
+++ src/pages/guest/RestaurantPage/OurShopPage/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import RestaurantSection from ".././RestaurantSection";
 import { Restaurant } from "~/hook/useRestaurantHook";
+import { Search } from "lucide-react";
 
 export default function OurShopPage() {
   const [searchInput, setSearchInput] = useState("");
@@ -46,26 +47,12 @@ export default function OurShopPage() {
           <form onSubmit={handleSearch}>
             <div className="mb-4 relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
+                <Search size={20} className="text-neutral-500" />
               </div>
               <input
                 type="search"
                 id="default-search"
-                className="block w-full p-4 ps-10 text-sm border rounded-xl"
+                className="block w-full p-4 ps-10 text-lg border rounded-2xl"
                 placeholder="Search for restaurants..."
                 value={searchInput}
                 onChange={handleSearchInputChange}
