61311510d89a96504b0522b9e05ebb6d8eb0e4ec
Muhammad Faried Risky <fariedrisky@gmail.com>
2023-11-16 14:29:21 +0700

    minor improve

--- src/pages/guest/AboutUsPage/index.tsx
+++ src/pages/guest/AboutUsPage/index.tsx
@@ -24,9 +24,6 @@ const AboutUsPage = () => {
                         <span className='font-medium'>
                             Our journey began with a vision to establish a distinctive culinary experience that fuses sustainability, exceptional service, and a lively atmosphere. Committed to our city's rich culinary heritage, we strive to respect our local roots while infusing a global commitment to reducing food waste.
                         </span>
-                        <span>
-                            At Place we believe that dining is not merely about the food but about the holistic experience. Our team, celebrated for their warmth and commitment, endeavors to make each visit a memorable occasion, all while championing our mission to combat food waste.
-                        </span>
                     </div>
                 </div>
             </div >
